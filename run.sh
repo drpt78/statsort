c94f602d99425183000e98302a6f9571eed85f77
Peter Taraba <peter.taraba.phd@gmail.com>
2026-03-25 16:47:26 -0700

    clean up

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-rm ./yo_example
-rm ./yo_test
+rm statsort_example
+rm statsort_test
 
-g++ -O2 statsort_example.cpp -I./include -o yo_example
-g++ -O2 statsort_test.cpp -I./include -o yo_test
+g++ -O2 statsort_example.cpp -I./include -o statsort_example
+g++ -O2 statsort_test.cpp -I./include -o statsort_test
 
-./yo_example
-./yo_test
+./statsort_example
+./statsort_test
