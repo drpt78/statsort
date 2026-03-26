024478135933e4bf28e38d08db29f39b48332c1d
Peter Taraba <peter.taraba.phd@gmail.com>
2026-03-26 00:18:56 -0700

    much better

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 rm statsort_example
 rm statsort_test
 
-g++ -O2 statsort_example2.cpp -I./include -o statsort_example
+g++ -O2 statsort_example.cpp -I./include -o statsort_example
 g++ -O2 statsort_test.cpp -I./include -o statsort_test
 
 ./statsort_example
