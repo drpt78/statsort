49443ad51a2f5864e149536554be46b2ca65b04c
Peter Taraba <peter.taraba.phd@gmail.com>
2026-03-28 05:33:49 -0700

    o3 is way faster than o2, hence claude wanted to use it as he most likely optimized code for o3. much much better. still messy code though.

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 rm statsort_example
-#rm statsort_test
+rm statsort_test
 
-g++ -O2 statsort_example.cpp -I./include -o statsort_example
-#g++ -O2 statsort_test.cpp -I./include -o statsort_test
+g++ -O3 statsort_example.cpp -I./include -o statsort_example
+g++ -O3 statsort_test.cpp -I./include -o statsort_test
 
 echo "------ example -----"
 ./statsort_example
 
-#echo "----- test -----"
-#./statsort_test
+echo "----- test -----"
+./statsort_test
